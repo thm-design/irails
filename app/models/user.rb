494af6c019614a53d66e306a200ffd10ce648f51
makoviy <artem.gooscha@gmail.com>
2013-02-25 13:21:30 +0200

    paperclip works

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
-  # has_attached_file :avatar,
-  #   styles: {
-  #     big: '256x256#',
-  #     medium: '64x64#',
-  #     small: '32x32#'
-  #   }
+  include Mongoid::Paperclip
+  has_mongoid_attached_file :avatar,
+    styles: {
+      big: '256x256#',
+      medium: '64x64#',
+      small: '32x32#'
+    }
 
   ## Database authenticatable
   field :email,              type: String, default: ""
